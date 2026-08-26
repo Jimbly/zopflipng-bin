18f9525f1d38c3b753f34c853f939456a24b6b00
Jimb Esser <wasteland@gmail.com>
2026-08-26 14:23:16 -0700

    Fix wrong path after moving where the logic lived

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const path = require('path');
 
-module.exports = path.join(__dirname, '../vendor',
+module.exports = path.join(__dirname, 'vendor',
 	process.platform === 'win32' ? 'win32' : process.platform === 'darwin' ? 'macos' : 'linux',
 	process.platform === 'win32' ? 'zopflipng.exe' : 'zopflipng');
