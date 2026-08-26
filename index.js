bcf6e753bc90abf5a33a53196ec86201ac8bc7a8
Jimb Esser <wasteland@gmail.com>
2026-08-26 14:07:59 -0700

    Fix wrong path format being exported

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import path from 'node:path';
 
-export default  path.join(import.meta.url, '../vendor',
+export default path.join(import.meta.dirname, '../vendor',
 	process.platform === 'win32' ? 'win32' : process.platform === 'darwin' ? 'macos' : 'linux',
 	process.platform === 'win32' ? 'zopflipng.exe' : 'zopflipng');
